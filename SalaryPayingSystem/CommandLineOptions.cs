3c7592a9c3101fbed672eddad0bcd4e52e9f046c
ngkaizhe <kaizhe1991@hotmail.com>
2024-07-04 23:55:27 +0800

    feat: add CommandLineParser package

---
+++ SalaryPayingSystem/CommandLineOptions.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+
+class CommandLineOptions
+{
+    [Value(0, MetaName = "EmpID", Required = true, HelpText = "Employee ID")]
+    public int EmployeeID { get; set; }
+
+    [Value(1, MetaName = "Name", Required = true, HelpText = "Employee name")]
+    public string Name { get; set; }
+
+    [Value(2, MetaName = "Address", Required = true, HelpText = "Employee address")]
+    public string Address { get; set; }
+
+    [Value(3, MetaName = "Salary", Required = true, HelpText = "Employee salary")]
+    public decimal Salary { get; set; }
+}
--- SalaryPayingSystem/Program.cs
+++ SalaryPayingSystem/Program.cs
@@ -1,3 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CommandLine;
 
-Console.WriteLine("Hello, World!");
+Parser.Default.ParseArguments<CommandLineOptions>(args)
+    .WithParsed(options =>
+    {
+        Console.WriteLine($"Employee ID: {options.EmployeeID}");
+        Console.WriteLine($"Name: {options.Name}");
+        Console.WriteLine($"Address: {options.Address}");
+        Console.WriteLine($"Salary: {options.Salary}");
+                
+        // Here you can perform further operations, such as adding the employee to a database
+        // or performing some other business logic.
+    })
+    .WithNotParsed(errors =>
+    {
+        // If there are any parsing errors, you can handle them here
+        // For example, print error messages or help text.
+        foreach (var error in errors)
+        {
+            Console.WriteLine(error.ToString());
+        }
+    });
