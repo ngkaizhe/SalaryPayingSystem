c6e53fd12e38d9be5bcfefaa634409c8b70c4f7e
ngkaizhe <kaizhe1991@hotmail.com>
2024-10-13 17:23:01 +0800

    chore: move SalesReceipt.cs to PayClassification folder

--- SalaryPayingSystem/Options/SalesReceipts/SalesReceipt.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SalaryPayingSystem.Options.SalesReceipts;
-
-public class SalesReceipt(DateTime date, double amount)
-{
-    public DateTime Date = date;
-    public double Amount = amount;
-}
---
+++ SalaryPayingSystem/PayClassifications/SalesReceipt.cs
@@ -0,0 +1,7 @@
+﻿namespace SalaryPayingSystem.PayClassifications;
+
+public class SalesReceipt(DateTime date, double amount)
+{
+    public DateTime Date = date;
+    public readonly double Amount = amount;
+}
